f828aa363e19a9b03ab1d836fff0b135083dae7d
Arong2013 <111150462+Arong2013@users.noreply.github.com>
2025-04-24 11:51:38 +0900

    데이터 베이스

---
+++ Assets/Scripts/Firebase/FirebaseDataBase.cs
@@ -0,0 +1,12 @@
+using Firebase;
+using Firebase.Database;
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FirebaseDataBase
+{
+    
+}
